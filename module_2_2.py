4e624f73ae94838913a82d46b84316a8c3bb3a77
Flyzja <mamontova-75@mail.ru>
2024-08-09 13:00:52 +0300

    для проекта №2

---
+++ module_2_2.py
@@ -0,0 +1,19 @@
+first = 123
+second = 456
+third = 789
+print(first, second, third)
+if first == second and first == third:
+    print(3)
+if first == second or second == third or second == first:
+    print(2)
+elif first != second and second != third and {second} != {first}:
+    print(0)
+
+
+
+
+
+
+
+
+
---
+++ черновик.py
@@ -0,0 +1,9 @@
+#def telechon(name, number):
+#    dict = {'Lera': 89531048074, 'Lyda': 89189781600}
+#telechon(dict[1], [])
+
+def count_vowels(in_string):
+    """Возвращает количество гласных в `in_string`"""
+    return sum(1 for char in in_string if char in "aeiouAEIOU")
+
+np.matrix = np.array([[1, 2], [3, 4]])
